cd71a4fd73da47a771f9df942253d3fa87157c05
AtomicFrenzy112 <66483196+AtomicFrenzy112@users.noreply.github.com>
2020-07-03 16:47:52 +0100

    Add files via upload

---
+++ Caeser cypher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Caesar_cypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter message to be encoded");
+            string encodingMessage=Console.ReadLine();
+            Console.WriteLine("Enter key");
+            int counter=0;
+            string output = "";
+            int key = Convert.ToInt32(Console.ReadLine());
+            char[]alphabetArray={'a','b','c','d','e',
+                'f','g','h','i','j','k','l','m','n','o','p',
+                'q','r','s','t','u','v','w','x','y','z'};
+            char[]charArr=encodingMessage.ToCharArray();
+            int k = 0;
+            int newChar = 0;
+            char[] encodedArray = charArr;
+            for (int i = 0; counter < encodingMessage.Length;i++)
+            {
+               
+                if (charArr[k] == alphabetArray[i])
+                {
+                    newChar = (i + key)%26;
+                    encodedArray[k] = alphabetArray[newChar];
+                    k++;
+                    counter = counter + 1;
+                    i = -1;
+                }
+            }
+            for(int s=0; s < encodedArray.Length; s++)
+            {
+                output = output + encodedArray[s];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
